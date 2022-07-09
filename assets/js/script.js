1f32f7c88fbbcd53bd7161c0fab3980726ca0d36
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-07-09 11:26:09 -0400

    created today box elements

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+var searchBoxEl = document.querySelector(".search-box");
+var searchHistoryEl = document.querySelector(".search-history");
+var weatherTodayEl = document.querySelector("#weather-today");
+var resultTitleEl = document.querySelector("#result-title");
+var resultTempEl = document.querySelector("#result-temp");
+var resultWindEl = document.querySelector("#result-wind");
+var resultHumidityEl = document.querySelector("result-humidity");
+var resultUVEl = document.querySelector("#result-UV");
+
+navigator.geolocation(position)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// searchBoxEl.addEventListener("submit", searchCity);
