692bb7455fc555e0250c3f46a3f83f765294e411
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-07-09 12:41:19 -0400

    fetch data from lat & long

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,15 @@ var resultWindEl = document.querySelector("#result-wind");
 var resultHumidityEl = document.querySelector("result-humidity");
 var resultUVEl = document.querySelector("#result-UV");
 
-navigator.geolocation(position)
+
+var cityNameEl = document.querySelector("textarea");
+
+
+
+//Function to covert address to Latitude and Longitude
+
+  //Call the function with address as parameter
+ 
 
 
 
@@ -23,9 +31,24 @@ navigator.geolocation(position)
 
 
 
+var searchCity = function() {
+    event.preventDefault();
+    var cityName = cityNameEl.value;
+    console.log(cityName);
 
+}
 
+var getCityData = function(lat, long) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&exclude=minutely,hourly,alerts&appid=f92fdda14af7b15ad0ec974e795f4725";
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data){
+                console.log(data);
+            });
+        };
+    });
+};
 
+getCityData(40.7128, 74.0060);
 
-
-// searchBoxEl.addEventListener("submit", searchCity);
+searchBoxEl.addEventListener("submit", searchCity);
